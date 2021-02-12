e6d11117b898b2d73ddd64160f8766fa719a79c7
BALLOON | FU-SEN <balloonakafusen@gmail.com>
2021-02-12 12:57:12 +0900

    😆 あっ、バグだ

--- bas.15j.run.php
+++ bas.15j.run.php
@@ -1,8 +1,9 @@
 <?php
 
 $path = @parse_url($_SERVER['REQUEST_URI'])['path'];
-
+$file = strtolower ( $path );
 $getfile = "../bas.15j.run/{$file}";
+
 echo "\x13\x0c\n";
 echo "NEW\n";
 require $getfile;
